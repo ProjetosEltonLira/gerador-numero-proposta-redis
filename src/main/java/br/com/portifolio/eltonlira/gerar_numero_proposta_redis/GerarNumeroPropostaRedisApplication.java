55cb617c30ceca717b06cf295ca82aa153173bfb
ProjetosEltonLira <eltondelira@gmail.com>
2025-11-20 11:58:12 -0300

    feature/criar-geracao-numero-proposta-com-redis

---
+++ src/main/java/br/com/portifolio/eltonlira/gerar_numero_proposta_redis/GerarNumeroPropostaRedisApplication.java
@@ -0,0 +1,13 @@
+package br.com.portifolio.eltonlira.gerar_numero_proposta_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerarNumeroPropostaRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerarNumeroPropostaRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/portifolio/eltonlira/gerar_numero_proposta_redis/GerarNumeroPropostaRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portifolio.eltonlira.gerar_numero_proposta_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerarNumeroPropostaRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
